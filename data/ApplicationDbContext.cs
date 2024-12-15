ff3d6c3c5dd759bbfd9c9273476710242e36b261
Jonas07 <jnthnpadillaleon072@gmial.com>
2024-12-15 18:25:42 -0400

    Agregando el mvc

---
+++ data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcSistema.Models;
+
+namespace mvcSistema.data
+{
+    public class ApplicationDbContext : DbContext // Aquí faltaba el ':'
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Aquí se incluirán todos los modelos que se creen
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
